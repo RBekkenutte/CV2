690831107415c15f526f14cab3ce645b29d1f056
Reinier Bekkenutte <reinierbekkenutte@hotmail.com>
2021-04-15 17:55:06 +0200

    ICP werkt nu echt

--- python.py
+++ python.py
@@ -1,7 +1,10 @@
 import numpy as np
 import open3d as o3d
+import scipy.io
 from scipy.io import loadmat
 from scipy.spatial import cKDTree as KDTree
+from sklearn.cluster import KMeans
+import time
 
 ######                                                           ######
 ##       notice: You don't need to strictly follow the steps         ##
@@ -29,12 +32,14 @@ vis_pcd.points = o3d.utility.Vector3dVector(pcd_arr_cleaned)
 
 
 A1 = pcd_arr_cleaned
+A1 = A1[A1[:, 2] < 2]
+
 ############################
 #   Load Data              #
 ############################
 ##  example
-
-pcd = o3d.io.read_point_cloud("Data/data/0000000025.pcd")
+target_file_name = "Data/data/0000000025"
+pcd = o3d.io.read_point_cloud(target_file_name + ".pcd")
 
 # ## convert into ndarray
 
@@ -44,29 +49,41 @@ pcd_arr = np.asarray(pcd.points)
 pcd_arr_cleaned = pcd_arr
 
 
+
 # visualization from ndarray
 vis_pcd = o3d.geometry.PointCloud()
 vis_pcd.points = o3d.utility.Vector3dVector(pcd_arr_cleaned)
 #o3d.visualization.draw_geometries([vis_pcd])
 
 A2 = pcd_arr_cleaned
+A2 = A2[A2[:, 2] < 2]
+
+
+
+
+# A2_normal_pcd = o3d.io.read_point_cloud(target_file_name +"_normal.pcd")
+# A2_normal= np.asarray(pcd.points)
+# breakpoint()
 
 ### Toy data
-A1 = loadmat("Data/source.mat")
+# A1 = loadmat("Data/source.mat")
 
-A1 = np.array(A1['source'])
-A1 = A1.T
-#A1 = o3d.geometry.PointCloud()
-#A1.points = o3d.Vector3dVector("Data/source.mat") 
+# A1 = np.array(A1['source'])
+# A1 = A1.T
+# #A1 = o3d.geometry.PointCloud()
+# #A1.points = o3d.Vector3dVector("Data/source.mat") 
 
-A2 = loadmat("Data/target.mat")
+# A2 = loadmat("Data/target.mat")
 
-A2 = np.array(A2['target'])
-A2 = A2.T
+# A2 = np.array(A2['target'])
+# A2 = A2.T
 
-#A2 = open3d.geometry.PointCloud()
+# A1 = np.array([[0,0,0], [30,30,30], [100,100,100]])
+# A2 = np.array([[10,10,10], [40,40,40], [110,110,110]])
 
-##A2.points = open3d.geometry.Vector3dVector("Data/target.mat") 
+#A2 = o3d.geometry.PointCloud()
+
+##A2.points = open3d.geometry.Vector3dVector("Data/target.mat") #
 
 ############################
 #     ICP                  #
@@ -95,64 +112,73 @@ t = np.reshape(t,(3,1))
 
 iteration = 0
 
-print(A2.shape)
 
+sampling = "Random_iterative"
 ### For random sampling
-#### Get 10 % random points of A2
-random_indices = np.random.choice(np.arange(A2.shape[0]), int(A2.shape[0]/10))
+sampling_size = 10000
 
-random_A2 = A2[random_indices,:]
-full_tree = KDTree(A2)
+
+if sampling == "All":
+    full_tree = KDTree(A2, leafsize = 8)
+if sampling == "Random":
+    random_indices = np.random.choice(np.arange(0,A2.shape[0]), sampling_size)
+    A2 = A2[random_indices]
+    full_tree = KDTree(A2, leafsize = 8)
+
+if sampling == "Bucket":
+    n_bins = 10
+    kmeans = KMeans(n_clusters=n_bins, random_state=0).fit(A2)
+    bins = kmeans.labels_
+    bin_array = []
+    for x in range(n_bins):
+        binn = np.argwhere(bins == x)
+        binn = np.reshape(binn, (binn.shape[0],))
+        bin_array.append(A2[binn])
+
 
 A1_transformed = A1
-sampling = "All"
+
+
 ###CHECK RMS
 RMS = 0 
 newRMS = 1
 while(np.absolute(newRMS - RMS) > 0.000001):
+#while(True):
     RMS = newRMS
     print("Iteration: ", iteration)
     
 
     ###### 3. transform point cloud with R and t
-
-
-    #print(A1)
-    #A1_transformed = np.matmul(A1,R) + t
-    #A1_transformed = np.zeros_like(A1, dtype=np.float64)
-
-    #print(A1_transformed)
-    # for i, point in enumerate(A1):
-    #     #point = np.unsqueeze(point,1)
-    #   #  print( point * R)
-    #     new =  point.dot(R.T) + t.T
-    #   #  print("new", new)
-    #     A1_transformed[i] = new
     
     A1_transformed = (R @ A1_transformed.T + t).T
+    #A1_transformed = R @ A1_transformed + t
 
-    if sampling == "All":
-        target = A2
-    if sampling == "Random":
-        target = random_A2
-    if sampling == "Random_iteration":
+
+    if sampling == "Random_iterative":
         ### Take a random sample each iteration
-        random_indices = np.random.choice(np.arange(A2.shape[0]), int(A2.shape[0]/10))
-        target = A2[random_indices,:]
-        
-
-    #vis_pcd.points = o3d.utility.Vector3dVector(A1_transformed)
-    #o3d.visualization.draw_geometries([vis_pcd])
-
-    ###### 4. Find the closest point for each point in A1 based on A2 using brute-force approach
-
+        random_indices = np.random.choice(np.arange(0,A2.shape[0]), sampling_size)
+        A2_iterative = A2[random_indices]
+        full_tree = KDTree(A2_iterative, leafsize = 8)
+    
+    
     distances, indices = full_tree.query(A1_transformed, k=1)
+
+    if sampling == "Bucket":
+        None
+        
+    
+    # if iteration % 5 == 0:    
+    #     color1 = np.ones_like(A1_transformed) * 0.3
+    #     color2 = np.ones_like(A2)  * 0.7
+    #     vis_pcd.points = o3d.utility.Vector3dVector(np.vstack([A1_transformed, A2]))
+    #     vis_pcd.colors = o3d.utility.Vector3dVector(np.vstack([color1, color2]))
+    #     o3d.visualization.draw_geometries([vis_pcd])
 
     
     #summation = 0
     #print(target.shape)
 
-    #A2_new = np.zeros_like(A1)
+    # A2_new = np.zeros_like(A1)
     # for j, pointA1 in enumerate(A1_transformed):
         
     #     dist = np.sum((target - pointA1)**2, axis=1)
@@ -172,6 +198,7 @@ while(np.absolute(newRMS - RMS) > 0.000001):
     #         print(j)
 
     newRMS = np.mean(np.power(distances, 2))
+    #newRMS = summation / A1_transformed.shape[0]
 
     print("RMSSSSSSSSSSSS", newRMS)
   
@@ -181,26 +208,19 @@ while(np.absolute(newRMS - RMS) > 0.000001):
     ### Center of mass
 
     A2_new = A2[indices]
+
+    
     centroid_1 = np.mean(A1_transformed, 0)
     centroid_2 = np.mean(A2_new, 0)
 
-
-    
-
-    A1_transformed = A1_transformed - centroid_1
-    A2_transformed = A2_new - centroid_2 
-
-       
-
     ### Covariance matrix
 
-    X = A1_transformed.T 
-    Y = A2_transformed.T
+    X = (A1_transformed - centroid_1).T 
+    Y = (A2_new - centroid_2 ).T
     #W = np.identity(X.shape[1])
 
-   # S = X @ W @ Y.T
+    #S = X @ W @ Y.T
     S = X @ Y.T
-
     #u , s, vh = np.linalg.svd(S, full_matrices=False, compute_uv=True)
     u , s, vh = np.linalg.svd(S)
 
@@ -211,7 +231,9 @@ while(np.absolute(newRMS - RMS) > 0.000001):
     
     R = vh.T @ matrix @ u.T
     t = centroid_2 - R @ centroid_1
+  #  t = [1000, 1000, 0]
     t = np.reshape(t,(3,1))
+
 
     iteration += 1
 
